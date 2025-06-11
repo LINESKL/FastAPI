4e4b0b18e5d12e6d069a80e3cfa7bbfaad9cb990
LINESKL <priveth06@gmail.com>
2025-06-12 01:28:18 +0500

    5 task

---
+++ Нурканат_Алиар_Защита_Эндпоинтов_5/main.py
@@ -0,0 +1,138 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from typing import Optional, Annotated
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+import os
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, UTC
+import uvicorn
+from fastapi.security import OAuth2PasswordBearer
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=True)
+session_factory = async_sessionmaker(bind=engine, expire_on_commit=False, autoflush=False)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+async def get_db():
+    async with session_factory() as session:
+        yield session
+
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
+
+class Base(DeclarativeBase):
+    pass
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+    yield
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(password, hashed_password)
+
+async def get_user(username: str, session: AsyncSession):
+    stmt = select(User).where(User.username == username)
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(UTC) + expires_delta
+    else:
+        expire = datetime.now(UTC) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(username, db)
+    if user is None:
+        raise credentials_exception
+    return user
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    username: str = Field(index=True, unique=True, min_length=3, max_length=50)
+    password: str = Field(min_length=8)
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+app1 = FastAPI(lifespan=lifespan)
+
+@app1.post("/register/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+async def register(user: UserCreate, session: SessionDep) -> UserOut:
+    db_user = await get_user(user.username, session=session)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already registered"
+        )
+    new_user = User(username=user.username, password=hash_password(user.password))
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
+
+@app1.post("/login/", response_model=Token)
+async def login(credentials: UserLogin, db: AsyncSession = Depends(get_db)):
+    user = await get_user(credentials.username, db)
+    if not user or not verify_password(credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app1.get("/users/me", response_model=UserOut)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
