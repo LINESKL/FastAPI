4477055502d8f507dfcbcb58fc76e78274c3679c
LINESKL <priveth06@gmail.com>
2025-06-24 16:08:19 +0500

    14 task

---
+++ Нурканат_Алиар_settings_14/index.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from metadata import lifespan
+from users import router as users_router
+from notes import router as notes_router
+from webs import router as ws_router
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from config.settings import settings
+
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(users_router)
+app.include_router(notes_router)
+app.include_router(ws_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=settings.CORS_METHODS,
+    allow_headers=settings.CORS_HEADERS,
+)
+if __name__ == "__main__":
+    uvicorn.run("index:app", reload=True)
